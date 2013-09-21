14140416427710a60f0c2dbb558c9d6085e66fc2
Jan Vermeir <jvermeir@xebia.com>
2013-09-21 19:56:02 +0200

    Renaming, clean up of Boot.

--- src/main/scala/service/Boot.scala
+++ src/main/scala/service/Boot.scala
@@ -7,15 +7,14 @@ import com.library.{FileBasedBookShelf, DutchPublicLibrary, Config}
 
 object Boot extends App {
 
+  private val HTTP_PORT: Int = 9181
   Config.libraryClient = new DutchPublicLibrary
   Config.bookShelf = new FileBasedBookShelf("data/boeken.dat")
 
   // TODO: Actor isn't restarted?
   implicit val system = ActorSystem("on-spray-can")
 
-  // create and start our service actor
-  val service = system.actorOf(Props[LibraryServiceActor], "demo-service")
+  val service = system.actorOf(Props[LibraryServiceActor], "library-service")
 
-  // start a new HTTP server on port 8080 with our service actor as the handler
-  IO(Http) ! Http.Bind(service, interface = "localhost", port = 9181)
+  IO(Http) ! Http.Bind(service, interface = "localhost", port = HTTP_PORT)
 }
